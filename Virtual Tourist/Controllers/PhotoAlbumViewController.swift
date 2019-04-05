e22c1ea0998c9518e9682721c2f5e68f08a0ab61
Mario Cezzare <mcezzare@gmail.com>
2019-04-04 22:45:49 -0300

    start project and setup storyboard screens

---
+++ Virtual Tourist/Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Mario Cezzare on 04/04/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoAlbumViewController:UIViewController {
+    
+}
---
+++ Virtual Tourist/Controllers/TravelLocationsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TravelLocationsViewController.swift
+//  Virtual Tourist
+//
+//  Created by Mario Cezzare on 04/04/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TravelLocationsViewController: UIViewController {
+    
+}
