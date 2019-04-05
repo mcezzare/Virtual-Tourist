16d78ac63e32cb2c4c1bc07601b0881131cedd42
Mario Cezzare <mcezzare@gmail.com>
2019-04-05 00:08:56 -0300

    created the PhotoViewCell.swift and connect in storyboard

---
+++ Virtual Tourist/Views/CustomView/PhotoViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoViewCell.swift
+//  Virtual Tourist
+//
+//  Created by Mario Cezzare on 05/04/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+
+import UIKit
+
+class PhotoViewCell: UICollectionViewCell {
+    static let identifier = "PhotoViewCell"
+    
+    var imageUrl: String = ""
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+}
