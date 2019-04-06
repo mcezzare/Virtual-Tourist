76f9757600398eecf5f65bd616c7659b117e7a5a
Mario Cezzare <mcezzare@gmail.com>
2019-04-06 01:03:31 -0300

    FIXED =)

--- Virtual Tourist/Views/CustomView/PhotoViewCell.swift
+++ Virtual Tourist/Views/CustomView/PhotoViewCell.swift
@@ -16,4 +16,27 @@ class PhotoViewCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    
+}
+
+
+extension PhotoViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = CGSize(width: 150, height: 150)
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout
+        collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
 }
